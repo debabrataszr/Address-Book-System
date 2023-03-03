b1d27b01ba10457f61d5e6bc5823381db127fee9
debabrata-97 <debabrataszr@gmail.com>
2023-03-03 19:44:41 +0530

    Create a Contact in Address Book

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -2,6 +2,26 @@ package com.bridgelabz;
 
 public class AddressBookMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Prgram");
+        System.out.println("Welcome to Address Book Program");
+
+        Contact c = new Contact();
+
+        c.firstname = "Debabrata";
+        c.lastname = "Sahoo";
+        c.address = "Koel Nagar";
+        c.city = "Rourkela";
+        c.state = "Odisha";
+        c.email = "debabratas@xyz.com";
+        c.zip = 751025;
+        c.phoneno = 7978;
+
+        System.out.println(c.firstname);
+        System.out.println(c.lastname);
+        System.out.println(c.address);
+        System.out.println(c.city);
+        System.out.println(c.state);
+        System.out.println(c.email);
+        System.out.println(c.zip);
+        System.out.println(c.phoneno);
     }
 }
---
+++ src/com/bridgelabz/Contact.java
@@ -0,0 +1,6 @@
+package com.bridgelabz;
+
+public class Contact {
+  String firstname,lastname,address,city,state,email;
+  int zip,phoneno;
+}
