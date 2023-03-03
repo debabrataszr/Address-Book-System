2851f7658951eb4abdeec87fa3ef9a08c6763660
Debabrata Priyadarshi Sahoo <124128301+debabrataszr@users.noreply.github.com>
2023-03-03 13:18:34 +0530

    Update AddressBookMain.java

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -2,6 +2,6 @@ package com.bridgelabz;
 
 public class AddressBookMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Prgram");
+        System.out.println("Welcome to Address Book Program");
     }
 }
