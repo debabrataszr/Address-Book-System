aa683d81670865367efc9fdfa8b4a00b488fd8a1
Debabrata Priyadarshi Sahoo <124128301+debabrataszr@users.noreply.github.com>
2023-03-03 20:19:16 +0530

    Update AddressBookMain.java

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -29,9 +29,7 @@ public class AddressBookMain {
             array[i] = contact;
         }
 
-        for (
-                int i = 0;
-                i < person; i++) {
+        for ( int i = 0; i < person; i++) {
             System.out.println(array[i].firstname + " " + array[i].lastname + " " + array[i].address + " "
                     + array[i].state + " " + array[i].zip + " " + array[i].phoneno + " " + array[i].email);
         }
